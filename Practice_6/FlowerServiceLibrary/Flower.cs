a5d04384f4712b0996ed002f83f21b2b6676d949
Dmitry <my.coding15944@gmail.com>
2020-04-22 15:22:34 +0300

    Added new field in flower

--- Practice_6/FlowerServiceLibrary/Flower.cs
+++ Practice_6/FlowerServiceLibrary/Flower.cs
@@ -9,9 +9,14 @@ namespace FlowerServiceLibrary
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Display(Name ="Название")]
+        [Display(Name ="Название цветка")]
         [Required(ErrorMessage ="Введите название цветка")]
         [StringLength(100,ErrorMessage ="Количество символом больше 100!")]
         public string Name { get; set; }
+
+        [Display(Name = "Название цветка на латинице")]
+        [Required(ErrorMessage = "Введите название цветка на латинице")]
+        [StringLength(100, ErrorMessage = "Количество символом больше 100!")]
+        public string LatinName { get; set; }
     }
 }
